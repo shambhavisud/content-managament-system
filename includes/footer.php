aaccc3f01ace1d64857e7df07992578c6b015a03
shambhavisud <67536519+shambhavisud@users.noreply.github.com>
2022-07-31 00:42:44 +0530

    comp sigs

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
    <footer>
        <div class="row">
            <div class="col-lg-12">
-               <p>Copyright &copy; CMS by Shambhavi Sud</p>
+               <p>Copyright &copy; HashComm by Shambhavi Sud</p>
            </div>
            <!-- /.col-lg-12 -->
        </div>
